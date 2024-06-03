f3e90609c5bd7c39bf5ad5789bb6afad51671433
Sdegraauw <144223601+Sdegraauw@users.noreply.github.com>
2024-06-03 10:07:16 +0200

    convert to mvc

---
+++ MVC omroep flevoland chat frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_omroep_flevoland_chat_frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
