a8aa12bd1d9137fde66a0eb916bef4b17d15b8ff
Sdegraauw <144223601+Sdegraauw@users.noreply.github.com>
2024-06-03 11:16:10 +0200

    width height detect

--- MVC omroep flevoland chat frontend/Controllers/HomeController.cs
+++ MVC omroep flevoland chat frontend/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MVC_omroep_flevoland_chat_frontend.Models;
 using System.Diagnostics;
+using System.Security.Cryptography;
 
 namespace MVC_omroep_flevoland_chat_frontend.Controllers
 {
